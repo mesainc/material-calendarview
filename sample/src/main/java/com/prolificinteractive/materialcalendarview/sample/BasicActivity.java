2d3a57626c84ec429cdf74100224594b749c30bf
Diego Silva <diegosilva@MacBook-Pro-de-Diego.local>
2016-12-29 16:43:37 -0300

    Removed hardcoded font

--- sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/BasicActivity.java
+++ sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/BasicActivity.java
@@ -36,6 +36,7 @@ public class BasicActivity extends AppCompatActivity implements OnDateSelectedLi
         setContentView(R.layout.activity_basic);
         ButterKnife.bind(this);
 
+        widget.setCalendarFont(R.string.font_futura_std_light);
         widget.setOnDateChangedListener(this);
         widget.setOnMonthChangedListener(this);
     }
