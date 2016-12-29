e6db0fb73b1c8e9ed98bedf85b98e0a81acf03b4
Diego Silva <diegosilva@MacBook-Pro-de-Diego.local>
2016-12-29 16:31:14 -0300

    Removed font initialization at MainActivity

--- sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/BasicActivity.java
+++ sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/BasicActivity.java
@@ -38,7 +38,6 @@ public class BasicActivity extends AppCompatActivity implements OnDateSelectedLi
         setContentView(R.layout.activity_basic);
         ButterKnife.bind(this);
 
-        widget.setCalendarFont(R.string.font_futura_std_bold);
         widget.setOnDateChangedListener(this);
         widget.setOnMonthChangedListener(this);
 
